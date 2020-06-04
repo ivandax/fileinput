082d15803a6c77a78d77e46873543b518eabd328
ivandax <ivandax_89@hotmail.com>
2020-06-04 18:38:16 +0200

    added validations, modified readme

--- src/components/ImageInput/ImageInput.js
+++ src/components/ImageInput/ImageInput.js
@@ -4,13 +4,28 @@ import './ImageInput.css';
 
 const ImageInput = ({getFile}) => {
 
-    const [message, setMessage] = useState(''); //to visually update status
-    const myInput = useRef();
+    const [message, setMessage] = useState(''); //to visually update status.
+    const myInput = useRef(); //the reference has to be set below.
+
+    //this is optional and can be expanded to validate other properties of the file.
+    const validate = (file) => {
+        if(file){
+            const acceptedExtensions = ['pdf','jpeg','jpg','gif','png']
+            console.log(file.name.split(".")[file.name.split(".").length-1])
+            if(acceptedExtensions.includes(file.name.split(".")[file.name.split(".").length-1].toLowerCase())){
+                return true;
+            } else{
+                return "File extension not supported."
+            };        
+        } else{
+            return "File not added."
+        }
+    };
 
     const handleChange = () => {
         setMessage(''); //reset message;
         const file = myInput.current.files[0];
-        if(file){
+        if(file && validate(file)===true){
             const reader = new FileReader();
             reader.readAsDataURL(file);
             reader.onload = () => {
@@ -20,7 +35,7 @@ const ImageInput = ({getFile}) => {
                 setMessage("File uploaded.")
             };
         } else{
-            setMessage("File not added.")
+            setMessage(validate(file)) //if false will show the message.
         }
     }
 
